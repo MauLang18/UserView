8857d4b166967ac43978ac48f63c6d7a1202399a
MauLang18 <maulangbonilla.18@gmail.com>
2024-01-15 10:36:12 -0600

    se cambio la url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,6 +3,6 @@ export const environment = {
   environment: "local",
   clientId:
     "687391569320-5u4rfrmkirviun959q8194jutqqf3t3e.apps.googleusercontent.com",
-  api: "http://190.113.124.155:9092/api/",
+  api: "http://190.113.124.155:9092/",
   filenameCsv: "dev-pos",
 };
