fb2478d75d2cf916582011574597f2e712cce57c
MauLang18 <maulangbonilla.18@gmail.com>
2024-02-23 08:23:35 -0600

    se cambiaron las url

--- src/app/shared/apis/endpoints.ts
+++ src/app/shared/apis/endpoints.ts
@@ -8,7 +8,7 @@ export const endpoints = {
   //USUARIO MODULE
   LIST_USUARIOS: "Usuario",
   USUARIO_BY_ID: "Usuario/",
-  REGISTER_USUARIO: "Usuario/Register/",
+  REGISTER_USUARIO: "Usuario/Register",
   EDIT_USUARIO: "Usuario/Edit/",
   REMOVE_USUARIO: "Usuario/Remove/",
 
@@ -16,7 +16,7 @@ export const endpoints = {
   LIST_ROL: "Rol",
   LIST_SELECT_ROL: "Rol/Select",
   ROL_BY_ID: "Rol/",
-  REGISTER_ROL: "Rol/Register/",
+  REGISTER_ROL: "Rol/Register",
   EDIT_ROL: "Rol/Edit/",
   REMOVE_ROL: "Rol/Remove/",
 
